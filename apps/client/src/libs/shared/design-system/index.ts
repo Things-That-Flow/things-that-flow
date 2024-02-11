5f05895ab579f45f7b750299a874e9bfe2a0e590
yongwoo <wooleejaan@gmail.com>
2024-02-11 16:48:06 +0900

    style: Add Ttflow's design system

---
+++ apps/client/src/libs/shared/design-system/index.ts
@@ -0,0 +1,91 @@
+import * as AccordionComponents from './ui/accordion'
+import * as AlertComponents from './ui/alert'
+import * as AlertDialogComponents from './ui/alert-dialog'
+import * as AspectRatioComponents from './ui/aspect-ratio'
+import * as AvatarComponents from './ui/avatar'
+import * as BadgeComponents from './ui/badge'
+import * as ButtonComponents from './ui/button'
+import * as CalendarComponents from './ui/calendar'
+import * as CardComponents from './ui/card'
+import * as CarouselComponents from './ui/carousel'
+import * as CheckboxComponents from './ui/checkbox'
+import * as CollapsibleComponents from './ui/collapsible'
+import * as CommandComponents from './ui/command'
+import * as ContextMenuComponents from './ui/context-menu'
+import * as DialogComponents from './ui/dialog'
+import * as DrawerComponents from './ui/drawer'
+import * as DropdownMenuComponents from './ui/dropdown-menu'
+import * as FormComponents from './ui/form'
+import * as HoverCardComponents from './ui/hover-card'
+import * as InputComponents from './ui/input'
+import * as LabelComponents from './ui/label'
+import * as MenubarComponents from './ui/menubar'
+import * as NavigationMenuComponents from './ui/navigation-menu'
+import * as PaginationComponents from './ui/pagination'
+import * as PopoverComponents from './ui/popover'
+import * as ProgressComponents from './ui/progress'
+import * as RadioGroupComponents from './ui/radio-group'
+import * as ResizableComponents from './ui/resizable'
+import * as ScrollAreaComponents from './ui/scroll-area'
+import * as SelectComponents from './ui/select'
+import * as SeparatorComponents from './ui/separator'
+import * as SheetComponents from './ui/sheet'
+import * as SkeletonComponents from './ui/skeleton'
+import * as SliderComponents from './ui/slider'
+import * as SonnerComponents from './ui/sonner'
+import * as SwitchComponents from './ui/switch'
+import * as TableComponents from './ui/table'
+import * as TabsComponents from './ui/tabs'
+import * as TextareaComponents from './ui/textarea'
+import * as ToastComponents from './ui/toast'
+import * as ToasterComponents from './ui/toaster'
+import * as ToggleComponents from './ui/toggle'
+import * as ToggleGroupComponents from './ui/toggle-group'
+import * as TooltipComponents from './ui/tooltip'
+import * as UseToastHooks from './ui/use-toast'
+
+export const TtflowAccordion = AccordionComponents
+export const TtflowAlert = AlertComponents
+export const TtflowAlertDialog = AlertDialogComponents
+export const TtflowAspectRatio = AspectRatioComponents
+export const TtflowAvatar = AvatarComponents
+export const TtflowBadge = BadgeComponents
+export const TtflowButton = ButtonComponents
+export const TtflowCalendar = CalendarComponents
+export const TtflowCard = CardComponents
+export const TtflowCarousel = CarouselComponents
+export const TtflowCheckbox = CheckboxComponents
+export const TtflowCollapsible = CollapsibleComponents
+export const TtflowCommand = CommandComponents
+export const TtflowContextMenu = ContextMenuComponents
+export const TtflowDialog = DialogComponents
+export const TtflowDrawer = DrawerComponents
+export const TtflowDropdownMenu = DropdownMenuComponents
+export const TtflowForm = FormComponents
+export const TtflowHoverCard = HoverCardComponents
+export const TtflowInput = InputComponents
+export const TtflowLabel = LabelComponents
+export const TtflowMenubar = MenubarComponents
+export const TtflowNavigationMenu = NavigationMenuComponents
+export const TtflowPagination = PaginationComponents
+export const TtflowPopover = PopoverComponents
+export const TtflowProgress = ProgressComponents
+export const TtflowRadioGroup = RadioGroupComponents
+export const TtflowResizable = ResizableComponents
+export const TtflowScrollArea = ScrollAreaComponents
+export const TtflowSelect = SelectComponents
+export const TtflowSeparator = SeparatorComponents
+export const TtflowSheet = SheetComponents
+export const TtflowSkeleton = SkeletonComponents
+export const TtflowSlider = SliderComponents
+export const TtflowSonner = SonnerComponents
+export const TtflowSwitch = SwitchComponents
+export const TtflowTable = TableComponents
+export const TtflowTabs = TabsComponents
+export const TtflowTextarea = TextareaComponents
+export const TtflowToast = ToastComponents
+export const TtflowToaster = ToasterComponents
+export const TtflowToggle = ToggleComponents
+export const TtflowToggleGroup = ToggleGroupComponents
+export const TtflowTooltip = TooltipComponents
+export const TtflowUseToast = UseToastHooks
