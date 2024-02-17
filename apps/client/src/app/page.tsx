9b180dd2d28842ac04ee4f5bbe5943d833f7a479
yongwoo <wooleejaan@gmail.com>
2024-02-17 20:09:47 +0900

    feat: Add 2D circle

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,5 +1,35 @@
+import Circle from '@/libs/circle/feature-circle/Circle'
+
 const Page = () => {
-  return <div>Welcome, Things That Flow World!!</div>
+  return (
+    <div
+      style={{
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}
+    >
+      <Circle
+        circleColor={{
+          r: 142,
+          g: 122,
+          b: 181,
+          a: 1
+        }}
+      />
+      <p
+        style={{
+          position: 'absolute',
+          color: 'white',
+          fontSize: '13px'
+        }}
+      >
+        Welcome, Things That Flow World!!
+      </p>
+    </div>
+  )
 }
 
 export default Page
