d1605657920190dfe2cdbd60c8e7f56a67b2e8b6
yongwoo <110705019+wooleejaan@users.noreply.github.com>
2024-02-17 20:18:16 +0900

    Add 2D circle using WebGL (#32)
    * feat: Add 2D circle
    
    * refactor: Add size prop

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,5 +1,39 @@
+import Circle from '@/libs/circle/feature-circle/Circle'
+
 const Page = () => {
-  return <div>Welcome, Things That Flow World!!</div>
+  return (
+    <div
+      style={{
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}
+    >
+      <Circle
+        size={{
+          width: 480,
+          height: 480
+        }}
+        color={{
+          r: 142,
+          g: 122,
+          b: 181,
+          a: 1
+        }}
+      />
+      <p
+        style={{
+          position: 'absolute',
+          color: 'white',
+          fontSize: '13px'
+        }}
+      >
+        Welcome, Things That Flow World!!
+      </p>
+    </div>
+  )
 }
 
 export default Page
