7da51acfd7c184c9a0a337785687e160590e7e11
yongwoo <wooleejaan@gmail.com>
2024-02-24 17:47:05 +0900

    chore(ttflow): Add configuration of commit-msg

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,7 +3,8 @@ export default {
   rules: {
     'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'test']],
     'scope-enum': [2, 'always', ['ttflow']],
-    'subject-full-stop': [2, 'never', '.'],
-    'subject-case': [0]
+    'scope-empty': [2, 'never'],
+    'subject-case': [0],
+    'header-trim': [0]
   }
 }
